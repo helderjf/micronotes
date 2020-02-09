083baf8afa9fb32bd428c05fd16cd0ca7086ba7f
Helder Faria <helderjf@gmail.com>
2020-02-09 22:44:19 +0000

    Update login response to return a dto containing token, fullname and email

--- src/main/java/com/hfaria/micronotesback/authentication/controller/AuthenticationController.java
+++ src/main/java/com/hfaria/micronotesback/authentication/controller/AuthenticationController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hfaria.micronotesback.authentication.dto.LoginRequestDTO;
+import com.hfaria.micronotesback.authentication.dto.LoginResponseDTO;
 import com.hfaria.micronotesback.authentication.dto.RegistryRequestDTO;
 import com.hfaria.micronotesback.authentication.service.AuthenticationService;
+import com.hfaria.micronotesback.model.User;
 
 @RestController
 @RequestMapping(path = "/api/auth")
@@ -34,8 +36,22 @@ public class AuthenticationController {
 	}
 	
 	@PostMapping("/login")
-	public String login(@RequestBody LoginRequestDTO loginRequest) {
-		return authService.login(loginRequest);
+	public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequest) {
+	    String token = authService.login(loginRequest);
+	    User user = getCurrentUser();
+	    LoginResponseDTO response = new LoginResponseDTO();
+	    response.fullName = user.getFullName();
+	    response.email = user.getEmail();
+	    response.authenticationToken = token;
+	    
+	    return response;
 	}
+	
+    private User getCurrentUser() {
+        User user = authService.getCurrentUser()
+                .orElseThrow(() -> new IllegalArgumentException("No user logged in."));
+        return user;
+    }
+	
 
 }
---
+++ src/main/java/com/hfaria/micronotesback/authentication/dto/LoginResponseDTO.java
@@ -0,0 +1,9 @@
+package com.hfaria.micronotesback.authentication.dto;
+
+public class LoginResponseDTO {
+    public String fullName;
+    public String email;
+    public String authenticationToken;
+    
+    
+}
