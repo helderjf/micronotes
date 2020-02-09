99b844dc91d422ed765baf9dcc098203b55c8b17
Helder Faria <helderjf@gmail.com>
2020-02-09 23:12:48 +0000

    Use keystore to sign and validate JWTs

--- src/main/java/com/hfaria/micronotesback/authentication/service/JWTService.java
+++ src/main/java/com/hfaria/micronotesback/authentication/service/JWTService.java
@@ -1,6 +1,14 @@
 package com.hfaria.micronotesback.authentication.service;
 
-import java.security.Key;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 
 import javax.annotation.PostConstruct;
 
@@ -10,38 +18,53 @@ import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-
-
 
 @Service
 public class JWTService {
-	
-	private Key key;
-	
-	
-	@PostConstruct
-	public void init() {
-		key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
-	}
 
-	public String getToken(Authentication authentication) {
-		User user = (User) authentication.getPrincipal();
-		return Jwts.builder()
-				.setSubject(user.getUsername())
-				.signWith(key)
-				.compact();
-	}
-	
-	public boolean validateToken(String token) {
-		Jwts.parser().setSigningKey(key).parseClaimsJws(token);
-		return true;
-	}
+    private KeyStore keyStore;
 
-	public String getSubjectFromJWT(String token) {
-		Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
-		return claims.getSubject();
-	}
-	
+    @PostConstruct
+    public void init() {
+        try {
+            InputStream stream = getClass().getResourceAsStream("/micronotes.jks");
+            keyStore = KeyStore.getInstance("JKS");
+            keyStore.load(stream, "123456".toCharArray());
+        } catch (NoSuchAlgorithmException | CertificateException | IOException | KeyStoreException e) {
+            throw new RuntimeException("Couldn't load keystore.");
+        }
+
+    }
+
+    public String getToken(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        return Jwts.builder().setSubject(user.getUsername()).signWith(getPrivateKey()).compact();
+    }
+
+    private PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey)keyStore.getKey("micronotes", "123456".toCharArray());
+        } catch (UnrecoverableKeyException | KeyStoreException | NoSuchAlgorithmException e) {
+            throw new RuntimeException("Couldn't get private key.");
+        }
+    }
+
+    public boolean validateToken(String token) {
+        Jwts.parser().setSigningKey(getPublicKey()).parseClaimsJws(token);
+        return true;
+    }
+
+    private PublicKey getPublicKey() {
+        try {
+            return keyStore.getCertificate("micronotes").getPublicKey();
+        } catch (KeyStoreException e) {
+            throw new RuntimeException("Couldn't get private key.");
+        }
+    }
+
+    public String getSubjectFromJWT(String token) {
+        Claims claims = Jwts.parser().setSigningKey(getPublicKey()).parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
+
 }
